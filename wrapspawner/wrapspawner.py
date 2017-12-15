e71bc9dc677eb15822feda8c00210f03dea9e28a
l.grazioli <luca.grazioli@icteam.it>
2017-12-15 18:07:31 +0100

    Definition of FilteredSpawner class and use of system default encoding for parsing groups string

--- wrapspawner/wrapspawner.py
+++ wrapspawner/wrapspawner.py
@@ -135,14 +135,17 @@ class ProfilesSpawner(WrapSpawner):
         2) administrator control of allowed configuration changes
         3) runtime choice of which Spawner backend to launch
     """
-    default_profiles = List(
-        trait = Tuple( Unicode(), Unicode(), Type(Spawner), Dict(), Unicode() ),
-        default_value = [],
+
+    profiles = List(
+        trait = Tuple( Unicode(), Unicode(), Type(Spawner), Dict() ),
+        default_value = [ ( 'Local Notebook Server', 'local', LocalProcessSpawner,
+                            {'start_timeout': 15, 'http_timeout': 10} ) ],
+        minlen = 1,
         config = True,
-        help = """List of profiles to offer in addition to docker images for selection. Signature is:
-                List(Tuple( Unicode, Unicode, Type(Spawner), Dict , Unicode)) corresponding to
-                profile display name, unique key, Spawner class, dictionary of spawner config options, comma separated list of authorized groups.
-                The first three values will be exposed in the input_template as {display}, {key}, and {type}"""
+        help = """List of profiles to offer for selection. Signature is:
+            List(Tuple( Unicode, Unicode, Type(Spawner), Dict )) corresponding to
+            profile display name, unique key, Spawner class, dictionary of spawner config options.
+            The first three values will be exposed in the input_template as {display}, {key}, and {type}"""
         )
 
     child_profile = Unicode()
@@ -189,7 +192,6 @@ class ProfilesSpawner(WrapSpawner):
 
     def select_profile(self, profile):
         # Select matching profile, or do nothing (leaving previous or default config in place)
-        print("Profiles type {}".format(type(self.profiles)))
         for p in self.profiles:
             if p[1] == profile:
                 self.child_class = p[2]
@@ -202,7 +204,7 @@ class ProfilesSpawner(WrapSpawner):
         super().construct_child()
 
     def load_child_class(self, state):
-        try: 
+        try:
             self.child_profile = state['profile']
         except KeyError:
             raise KeyError('jupyterhub database might be outdated, please reset it, in the default configuration, just delete jupyterhub.sqlite')
@@ -216,12 +218,32 @@ class ProfilesSpawner(WrapSpawner):
     def clear_state(self):
         super().clear_state()
         self.child_profile = ''
+
+class FilteredSpawner(ProfilesSpawner):
+
+    """ProfilesSpawner - leverages the Spawner options form feature to allow user-driven
+        configuration of Spawner classes while permitting:
+        1) configuration of Spawner classes that don't natively implement options_form
+        2) administrator control of allowed configuration changes
+        3) runtime choice of which Spawner backend to launch
+    """
+    default_profiles = List(
+        trait = Tuple( Unicode(), Unicode(), Type(Spawner), Dict(), Unicode() ),
+        default_value = [],
+        config = True,
+        help = """List of profiles to offer in addition to docker images for selection. Signature is:
+                List(Tuple( Unicode, Unicode, Type(Spawner), Dict , Unicode)) corresponding to
+                profile display name, unique key, Spawner class, dictionary of spawner config options, comma separated list of authorized groups.
+                The first three values will be exposed in the input_template as {display}, {key}, and {type}"""
+        )
     
     def get_user_groups(self):
-        import subprocess
+        import subprocess, sys
         user = self.user.name
         cmd_result = subprocess.Popen("groups "+user, shell=True, stdout=subprocess.PIPE).stdout.read()
-        groups_list = cmd_result.decode('ascii').split(':')[1].replace('\n', '').strip().split(' ')
+        #Get system default encoding
+		sys_encoding = sys.getdefaultencoding()
+		groups_list = cmd_result.decode(sys_encoding).split(':')[1].replace('\n', '').strip().split(' ')
         return groups_list
     
     def _user_profiles(self):
@@ -242,7 +264,7 @@ class ProfilesSpawner(WrapSpawner):
     def profiles(self):
         #New profiles are default profiles in addition to user_based profiles
         return self._user_profiles()
-
+		
 class DockerProfilesSpawner(ProfilesSpawner):
 
     """DockerProfilesSpawner - leverages ProfilesSpawner to dynamically create DockerSpawner
